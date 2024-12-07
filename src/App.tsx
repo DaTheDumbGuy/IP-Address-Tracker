59eed3507d3084d04f19dd6bc6d81910b85ef072
Daryl Bacurin <darylbacurin14@gmail.com>
2024-12-07 23:56:23 +0800

    Update API, App, vite/tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>IP Address Tracker</h1>
+        <div>
+          <input type="text" />
+          <button type="button">Button</button>
+        </div>
+      </header>
+      <main></main>
+    </>
+  )
+}
